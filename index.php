428238033916c69f43746d13422bb45e69ca2f87
Cesar Reyes <soporte4@zorasystems.com>
2023-04-18 23:09:18 -0500

    subiendo archivos inicio

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario</title>
+	<link rel="stylesheet" type="text/css" href="CSS/bootstrap.css">
+    <script src="JS/jquery-3.6.4.min.js" type="text/javascript"></script>
+    <script src="JS/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+    <h1> Bienvenido </h1>
+    <form>
+        <label for="usuario">Usuario</label> 
+        <input type="text" name="usuario" id="usuario">
+        <br>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password">   
+        <button>Ingresar</button>
+    </form>
+</body>
+</html>
