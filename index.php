3e2e4b85c4214a48ebbe2bd9b019d3ede3592480
Cesar Reyes <soporte4@zorasystems.com>
2023-04-20 10:41:42 -0500

    implementado php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-    
+    if(isset($_POST['usuario'])){
+        echo $_POST['usuario'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,15 +14,25 @@
     <script src="JS/jquery-3.6.4.min.js" type="text/javascript"></script>
     <script src="JS/bootstrap.min.js" type="text/javascript"></script>
 </head>
+
 <body>
-    <h1> Bienvenido </h1>
-    <form>
-        <label for="usuario">Usuario</label> 
-        <input type="text" name="usuario" id="usuario">
-        <br>
-        <label for="password">Contraseña</label>
-        <input type="password" name="password" id="password">   
-        <button>Ingresar</button>
-    </form>
+
+    <h1 class="text-center"> Bienvenido </h1>
+    <div class="row">
+        <div class="mx-auto col-10 col-md-8 col-lg-6">
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <div class="form-group">
+                    <label for="usuario">Usuario</label> 
+                    <input class="form-control" type="text" name="usuario" id="usuario">
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña</label>
+                    <input class="form-control" type="password" name="password" id="password">   
+                </div>
+                <button type="submit" class="btn btn-primary">Ingresar</button>
+            </form>
+        </div>
+    </div>
+
 </body>
 </html>
